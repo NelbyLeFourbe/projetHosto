b75d2f6c9c28c8c59c3eecc03faf9721e920fdb9
Unknown <terence.allart@gmail.com>
2018-09-11 16:01:58 +0200

    maj

--- hopital/src/main/java/sopra/promo404/hopital/repository/IRepositoryConsultation.java
+++ hopital/src/main/java/sopra/promo404/hopital/repository/IRepositoryConsultation.java
@@ -3,6 +3,7 @@ package sopra.promo404.hopital.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import sopra.promo404.hopital.model.Consultation;
 import sopra.promo404.hopital.model.Patient;
@@ -11,7 +12,7 @@ import sopra.promo404.hopital.model.Patient;
 public interface IRepositoryConsultation extends JpaRepository<Consultation, Long> {
 
 	List<Consultation> findAllConsultation();
-
+	@Query("select c from Consultation c left join fetch c.salles s where c.id = :id")
 	Patient findByIdWithSalles(Long id);
 
 }
