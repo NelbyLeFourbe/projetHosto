1993a75419a2024287859eedd4c2ee8e30632f0b
Unknown <valentin.peclard@hotmail.fr>
2018-09-11 14:06:00 +0200

    start

---
+++ hopital/src/test/java/sopra/promo404/hopital/HopitalApplicationTests.java
@@ -0,0 +1,33 @@
+package sopra.promo404.hopital;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import sopra.promo404.hopital.model.Patient;
+import sopra.promo404.hopital.repository.IRepositoryPatient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HopitalApplicationTests {
+
+	@Autowired
+	private IRepositoryPatient repoPatient;
+
+	@Test
+	public void contextLoads() {
+
+		Patient gerard = new Patient();
+		gerard.setNom("Gerard");
+
+		repoPatient.save(gerard);
+		
+		Patient gilles = new Patient();
+		gilles.setNom("Gilles");
+
+		repoPatient.save(gilles);
+		
+	}
+}
